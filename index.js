7c3b556f6f2c370a341f6612c20c05bc3481d63e
Emoncr <biplobemon 75945@gmail.com>
2023-09-17 10:43:14 +0600

    DB connected

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
 port = 5000;
 
-
-
-
-
-
-
-
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("connected to DB");
+})
+.catch((err)=>{
+console.log(err); 
+})
 
 app.listen(port, () => {
   console.log("Listing from port 5000");
